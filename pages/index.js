aefddd461027e63f37164d240347b0bac19a16f1
s0npaRi11 <paragmahale36@gmail.com>
2021-08-25 19:38:59 +0530

    home page redirection

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,10 @@ import { useRouter } from 'next/router'
 
 export default function Home() {
 
+  const router = useRouter()
   useEffect(() => {
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const router = useRouter()
     router.push('/tv-shows/avengers')
     
   },[])
